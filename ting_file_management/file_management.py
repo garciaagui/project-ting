b25c79839c73b16b7106382bba9338cdeda530bd
Guilherme Garcia <garciaguig@gmail.com>
2023-05-06 15:40:52 -0300

    feat: Implements `txt_importer` function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        with open(path_file, "r") as f:
+            lines = f.read().split("\n")
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    else:
+        return lines
