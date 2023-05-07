2304b6defeeca10bfe5cbf1209591597f535d7aa
Guilherme Garcia <garciaguig@gmail.com>
2023-05-07 17:01:14 -0300

    feat: Implements `exists_word` function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = list()
+
+    for index in range(instance.__len__()):
+        archive = instance.search(index)
+        ocurrences = list()
+
+        for index, line in enumerate(archive['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                ocurrences.append({"linha": index + 1})
+
+        if ocurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": archive['nome_do_arquivo'],
+                "ocorrencias": ocurrences,
+            })
+
+    return results
 
 
 def search_by_word(word, instance):
