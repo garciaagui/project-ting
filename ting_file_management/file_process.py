39be4a4e69b3ff3260832081da3f56cb0684085e
Guilherme Garcia <garciaguig@gmail.com>
2023-05-07 12:27:56 -0300

    feat: Implements `process` function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(instance.__len__()):
+        value = instance.search(index)
+        if value["nome_do_arquivo"] == path_file:
+            return None
+
+    lines = txt_importer(path_file)
+    value_to_enqueue = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(value_to_enqueue)
+    print(f"{value_to_enqueue}", file=sys.stdout)
 
 
 def remove(instance):
