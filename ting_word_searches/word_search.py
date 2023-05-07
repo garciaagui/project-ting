e2d8d8d4c9215f90bd12e58d6703e22ab603e54a
Guilherme Garcia <garciaguig@gmail.com>
2023-05-07 17:22:24 -0300

    refactor: Implements `get_ocurrences` and adds it on `exists_word`

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,13 +1,26 @@
+def get_ocurrences(word, archive, function):
+    ocurrences = list()
+
+    for index, line in enumerate(archive['linhas_do_arquivo']):
+        if word.lower() in line.lower():
+            value = (
+                {"linha": index + 1}
+                if function == 'exists_word'
+                else {"linha": index + 1, "conteudo": line}
+            )
+
+            ocurrences.append(value)
+
+    return ocurrences
+
+
 def exists_word(word, instance):
     results = list()
 
     for index in range(instance.__len__()):
         archive = instance.search(index)
-        ocurrences = list()
 
-        for index, line in enumerate(archive['linhas_do_arquivo']):
-            if word.lower() in line.lower():
-                ocurrences.append({"linha": index + 1})
+        ocurrences = get_ocurrences(word, archive, "exists_word")
 
         if ocurrences:
             results.append({
