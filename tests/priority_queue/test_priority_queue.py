dd2b8a065f493bb9fe1e4665657ea29ad0ee7ca3
Guilherme Garcia <garciaguig@gmail.com>
2023-05-08 10:05:00 -0300

    test: Add tests for `PriorityQueue` class

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,43 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    mocked_values = [
+        {
+            "nome_do_arquivo": "test3.txt",
+            "qtd_linhas": 6,
+            "linhas_do_arquivo": [1, 2, 3, 4, 5, 6]
+        },
+        {
+            "nome_do_arquivo": "test2.txt",
+            "qtd_linhas": 1,
+            "linhas_do_arquivo": [1]
+        },
+        {
+            "nome_do_arquivo": "test1.txt",
+            "qtd_linhas": 10,
+            "linhas_do_arquivo": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        },
+        {
+            "nome_do_arquivo": "test4.txt",
+            "qtd_linhas": 4,
+            "linhas_do_arquivo": [1, 2, 3, 4]
+        },
+    ]
+
+    priority_queue = PriorityQueue()
+
+    for value in mocked_values:
+        priority_queue.enqueue(value)
+
+    assert len(priority_queue) == len(mocked_values)
+
+    dequeued_value = priority_queue.dequeue()
+    assert dequeued_value == mocked_values[1]
+
+    value_found = priority_queue.search(0)
+    assert value_found == mocked_values[3]
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(100)
